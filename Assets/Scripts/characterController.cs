1677bcb2cf57dc941f3c4fbf40a596d1057e825e
Kattnos <kattnoskanal@gmail.com>
2021-08-09 12:59:01 +0200

    Improved crouching

--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -15,19 +15,20 @@ public class characterController : MonoBehaviour
 	public bool isCrouching = false;
 	private float timer;
 	private bool firstCrouch = true;
+	private float t;
+	public float crouchLength;
+	public float currentPosZ;
+	private bool crouchDone;
+	private bool stopCrouching;
+	private float temp;
+	private float standardPosZ = 0.0066f;
 	private void Start()
 	{
 		charControl = GetComponent<CharacterController>();
-		Camera = GameObject.Find("PlayerCamera");
+		Camera = GameObject.Find("PlayerCamera"); 
 	}
-	private void FixedUpdate()
-	{
-		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-		move = Camera.transform.TransformDirection(move);
-		charControl.Move(move * speed * Time.deltaTime);
-		velocity.y += gravity * Time.deltaTime;
-		charControl.Move(velocity * Time.deltaTime);
-
+	private void Update () {
+		currentPosZ = Camera.transform.localPosition.z;
 		if (holdCrouch == true)
 		{
 			if (Input.GetAxis("Crouch") == 1) {
@@ -50,12 +51,30 @@ public class characterController : MonoBehaviour
 				isCrouching = false;
 			}
 		}
-		if (isCrouching == true)
+		if (isCrouching == true && crouchDone == false)
 		{
-			
-		} else
+			t += Time.deltaTime;
+			Camera.transform.localPosition = new Vector3(Camera.transform.localPosition.x, Camera.transform.localPosition.y, Mathf.Lerp((currentPosZ -= crouchLength), currentPosZ, t));
+			if (Camera.transform.localPosition.z == standardPosZ - crouchLength) {
+				crouchDone = true;
+				stopCrouching = true;
+			}
+		} else if (isCrouching == false && stopCrouching == true)
 		{
-
+			Debug.Log(standardPosZ);
+			Camera.transform.localPosition = new Vector3(Camera.transform.localPosition.x, Camera.transform.localPosition.y, Mathf.Lerp(currentPosZ, (currentPosZ += crouchLength), t));
+			if (Camera.transform.localPosition.z >= standardPosZ + crouchLength) {
+				stopCrouching = false;
+				Debug.Log("This triggers too");
+			}
 		}
+	}
+	private void FixedUpdate()
+	{
+		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		move = Camera.transform.TransformDirection(move);
+		charControl.Move(move * speed * Time.deltaTime);
+		velocity.y += gravity * Time.deltaTime;
+		charControl.Move(velocity * Time.deltaTime);
 	}
 }
