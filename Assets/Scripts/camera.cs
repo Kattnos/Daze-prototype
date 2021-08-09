a0af2e51db86b191ae752e7dba9e8f3b2afb2a8a
Kattnos <kattnoskanal@gmail.com>
2021-08-09 09:53:43 +0200

    Initial Commit

---
+++ Assets/Scripts/camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+	public float horizontal;
+	public float vertical;
+	private float yaw = 0f;
+	private float pitch = 0f;
+
+	private void Update()
+	{
+		yaw += horizontal * Input.GetAxis("Mouse X");
+		pitch -= vertical * Input.GetAxis("Mouse Y");
+		pitch = Mathf.Clamp(pitch, -80f, 80f);
+
+		transform.eulerAngles = new Vector3(pitch, yaw, 0f);
+	}
+}
---
+++ Assets/Scripts/characterController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class characterController : MonoBehaviour
+{
+	//Fix toggle crouch bug with timer
+    private CharacterController charControl;
+	public int speed;
+	public float gravity;
+	private Vector3 velocity;
+	private GameObject Camera;
+	public bool holdCrouch;
+	Quaternion eulerAngle;
+	public bool isCrouching = false;
+	private float timer;
+	private bool firstCrouch = true;
+	private void Start()
+	{
+		charControl = GetComponent<CharacterController>();
+		Camera = GameObject.Find("PlayerCamera");
+	}
+	private void FixedUpdate()
+	{
+		Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		move = Camera.transform.TransformDirection(move);
+		charControl.Move(move * speed * Time.deltaTime);
+		velocity.y += gravity * Time.deltaTime;
+		charControl.Move(velocity * Time.deltaTime);
+
+		if (holdCrouch == true)
+		{
+			if (Input.GetAxis("Crouch") == 1) {
+				isCrouching = true;
+			} else
+			{
+				isCrouching = false;
+			}
+		} else if (holdCrouch == false)
+		{
+			timer += Time.deltaTime;
+			if (Input.GetAxis("Crouch") == 1 && isCrouching == false && timer >= 0.05f)
+			{
+				firstCrouch = false;
+				timer = 0f;
+				isCrouching = true;
+			} else if (Input.GetAxis("Crouch") == 1 && isCrouching == true && timer >= 0.05f && firstCrouch == false)
+			{
+				timer = 0f;
+				isCrouching = false;
+			}
+		}
+		if (isCrouching == true)
+		{
+			
+		} else
+		{
+
+		}
+	}
+}
